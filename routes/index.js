f3e697709c196603e1bdbc4d95d82168de055e73
nitish saini <nitish.saini@originscale.io>
2024-07-31 12:52:16 +0530

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const userRoutes = require('./userRoutes');
-const discussionRoutes = require('./discussionRoutes');
 const commentRoutes = require('./blogs');
 const authRoutes = require('./authRoutes');
 const multer = require('multer');
-const path = require('path');
 const auth = require('../utils/auth');
+const AWS = require('aws-sdk');
+const dotenv = require('dotenv');
 
+dotenv.config(); 
 router.use('/api/users', userRoutes);
 router.use('/api/blogs', commentRoutes);
 router.use('/api/auth', authRoutes);
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        console.log('file', file)
-        cb(null, 'uploads/'); 
-    },
-    filename: (req, file, cb) => {
-        console.log('file2', file)
-        cb(null, Date.now() + path.extname(file.originalname)); 
-    },
-});
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage });
 
-const upload = multer({ storage: storage });
+const s3 = new AWS.S3({
+    endpoint: new AWS.Endpoint(process.env.s3_url),
+    accessKeyId: process.env.access_key,
+    secretAccessKey: process.env.secret_key,
+    s3ForcePathStyle: true
+  });
 
-router.post('/api/upload', auth,upload.single('image'), (req, res, next) => {
-    try {
-        console.log('req', req.file)
-        if (!req.file) {
-            return res.status(400).json({ message: 'No file uploaded.' });
-        }
 
-        // File upload successful
-        res.status(201).json({
-            message: 'image uploaded successfully',
-            file: req.file, 
-            imageLink:`${req.protocol}://${req.get('host')}/${req.file.path}`
-        });
-    } catch (error) {
-        console.error('Error uploading file:', error);
-        res.status(500).json({ message: 'Error uploading file' });
+router.post('/upload', upload.single('image'), (req, res) => {
+    if (!req.file) {
+      return res.status(400).send('No file uploaded.');
     }
-});
+  
+    const params = {
+      Bucket: process.env.bucket_name,
+      Key: req.file.originalname,
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype,
+      ACL: 'public-read',
+    };
+  
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.error('Error uploading to Cloudflare R2:', err);
+        return res.status(500).send('Error uploading file.');
+      }
+      const imageUrl = `${process.env.public_url}/${params.Bucket}/${params.Key}`;
+      res.status(200).send({ imageUrl: imageUrl });
+    });
+  });
 module.exports = router;
