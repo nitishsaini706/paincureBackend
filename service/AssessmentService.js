eca6c7358be6a789869266abe78443d80c99bf45
Aryan Malpotra <aryan.malpotra@originscale.io>
2024-08-08 19:17:52 +0530

    changes

--- service/AssessmentService.js
+++ service/AssessmentService.js
@@ -2,8 +2,7 @@
 const {pool} = require('../db/connection')
 
 async function createAssessmentForm(userData) {
-  const { email, name, age, gender ,companyName, designation, sitTime, exerciseTime, physicalActivity, sleepTime, medicalCondition,
-          medications, surgeries, servicesInterest, goals } = userData;
+  
 
   try {
     const client = await pool.connect();
@@ -11,8 +10,7 @@ async function createAssessmentForm(userData) {
 
     const query = 'INSERT INTO assessments(email, full_name, age, gender, company_name, designation, sit_time, exercise_time, physical_activity,' +
     'sleep_time, medical_condition, medications, surgeries, services_interest, goals) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING id';
-    const values = [email, name, age, gender ,companyName, designation, sitTime, exerciseTime, physicalActivity, sleepTime, medicalCondition,
-                    medications, surgeries, servicesInterest, goals];
+    const values = [userData];
 
     const { rows } = await client.query(query, values);
     const data = rows[0];
