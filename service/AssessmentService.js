af5c0e85047342f85f359112b15a8bb8ad38fec2
Aryan Malpotra <aryan.malpotra@originscale.io>
2024-08-09 20:01:02 +0530

    changes

--- service/AssessmentService.js
+++ service/AssessmentService.js
@@ -7,13 +7,14 @@ async function createAssessmentForm(userData) {
   try {
     const client = await pool.connect();
     console.log('Connected to PostgreSQL');
+    console.log("userData",userData)
      const { fullName, height, weight, email, age, gender, companyName, designation, sittingHours, exercise, exerciseFrequency, activities, medicalConditions, medications, sleepHours,
        surgeries, services, goals } = userData;
     const query = 'INSERT INTO assessments(email, full_name, age, gender, company_name, designation, sit_time, exercise_time, physical_activity,' +
     'sleep_time, medical_condition, medications, surgeries, services_interest, goals, height, weight) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING id';
     // const query = 'INSERT INTO assessments(services_interest) VALUES ($1) RETURNING id';
     const values = [email, fullName, age, gender, companyName, designation, sittingHours, exercise, activities, sleepHours,medicalConditions, medications, surgeries, services, goals, height, weight ];
-
+  console.log("query",query)
     const { rows } = await client.query(query, values);
     const data = rows[0];
 
